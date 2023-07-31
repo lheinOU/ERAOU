371c6acb2a027ac59e92a2bf69eacb6498834473
lheinOU <84026936+lheinOU@users.noreply.github.com>
2023-07-31 00:20:24 -0400

    Upload
    Uploading

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set("/", "html");
+app.use(express.static(path.join(__dirname,"/")));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.get('/', (req,res) => {
+    res.render('index');
+
+});
+
+app.listen(3210, () => {
+console.log("Listening on http://localhost:3210")
+});
